76153e9bb1966807c44899264831ea1324e785b3
ALTERRQ <alterrq@gmail.com>
2025-08-30 12:41:09 +0200

    Added Kali dependencie install support and repo update before build

--- build/ckbuild.sh
+++ build/ckbuild.sh
@@ -113,7 +113,7 @@ DTS_OC="$KDIR/arch/arm64/boot/dts/exynos/exynos2100_oc.dts"
 # Dependencies
 UB_DEPLIST="lz4 brotli flex bc cpio kmod ccache zip binutils-aarch64-linux-gnu device-tree-compiler curl bison libssl-dev python-is-python3"
 if [ ! -f ".flag" ]; then
-  if grep -q -E "Ubuntu|Debian" /etc/os-release; then
+  if grep -q -E "Ubuntu|Debian|Kali" /etc/os-release; then
     sudo apt install $UB_DEPLIST -y
       if [ $? -ne 0 ]; then
         echo -e "$RED\nERROR: Failed to install dependencies\n$ENDCOLOR"
--- do_build.sh
+++ do_build.sh
@@ -3,4 +3,8 @@
 # Wrap to ckbuild.sh
 export WP=${WP:-$(realpath $PWD/../)}
 
+# Update repo & Update/install git submodules
+git pull
+git submodule update --init --recursive
+
 bash build/ckbuild.sh "$@"  > >(tee log.txt) 2>&1
